fdbc07d50d5b4bd8d7fab58e24b6e08180f9c2f8
Adrian Tortajada <atortajada@ganaenergia.com>
2021-06-16 16:45:22 +0200

    a

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+const https = require("https");
 
 
 class Dame {
@@ -22,11 +23,65 @@ class Dame {
     };
     
     
-    get() {};
-    post() {};
-    put() {};
-    patch() {};
-    delete() {};
+    get(url, options) {
+		
+		const req = https.request({
+			hostname: this.config.default.baseUrl,
+			port: 443,
+			path: url,
+			method: "GET",
+		}, (res) => {
+			
+			console.log("statusCode:", res.statusCode);
+			console.log("headers:", res.headers);
+
+			res.on("data", (d) => {
+				process.stdout.write(d);
+			});
+		});
+		
+		req.on("error", (e) => {
+			console.error(e);
+		});
+		
+		req.end();
+		
+	};
+	
+	
+	
+    post(url, body, options) {
+		
+		
+		
+	};
+	
+	
+	
+    put() {
+		
+		
+		
+	};
+	
+	
+	
+    patch() {
+		
+		
+		
+	};
+	
+	
+	
+    delete() {
+		
+		
+		
+	};
+	
+	
+	
 	
 	
 	/**
@@ -43,69 +98,4 @@ class Dame {
 };
 
 const dame = new Dame();
-
-
-dame.setConfig("default", "baseUrl", "https://ganaenergia.com/api");
-dame.setConfig("default", "token", "JWT");
-dame.setConfig("crm", "baseUrl", "https://crm.ganaenergia.com");
-dame.setConfig("crm", "token", "JWT_2");
-
-
-
-const {response, isError} = dame.post("/algo", objAlgo);
-if (isError) return console.log(response.message);
-
-
-// 200
-res200 = {
-    response: {
-        message: "whatever",
-    },
-    code: 200,
-    status: "OK",
-	isError: false,
-};
-
-// 500
-res500 = {
-    response: null,
-    code: 500,
-    status: "Internal server error server error",
-	isError: true,
-};
-// 500
-res500_2 = {
-    response: {
-		message: {
-			error: "mongoerror: sadjkldjasklja"
-		}
-	},
-    code: 500,
-    status: "Internal server error server error",
-	isError: true,
-};
-
-
-
-const {response, code, status} = dame.post("/algoCrm", {
-    user: "asd",
-    password: "asd",
-}, {
-	config: "crm", // override config
-});
-
-
-
-const {response, code, status} = dame.post("https://api.neuro.com/dameDatos", {
-    user: "asd",
-    password: "asd",
-}, {
-	useToken: false,
-	ignoreBaseUrl: true,
-	force: {
-		token: "token_neuro",
-	}
-});
-
-
-
+module.exports = dame;
--- playground.js
+++ playground.js
@@ -1,23 +1,75 @@
 
-// Import
-const {validame, validameConfig} = require("./index");
-
-validameConfig.language = "es";
+const dame = require("./index");
 
 
 
-let error = validame("mike", {
-	allow: "1 a"
-});
+dame.setConfig("default", "baseUrl", "https://ganaenergia.com/api");
+dame.setConfig("default", "token", "JWT");
+dame.setConfig("crm", "baseUrl", "https://crm.ganaenergia.com");
+dame.setConfig("crm", "token", "JWT_2");
 
 
 
-if (error === "") {
-	console.log( "playground ✅" );
-} else {
-	console.log("playground ❌", error );
+console.log( "dame", `(${typeof dame}): `, dame);
+
+return;
+
+
+
+const {response, isError} = dame.post("/algo", objAlgo);
+if (isError) return console.log(response.message);
+
+
+// 200
+res200 = {
+    response: {
+        message: "whatever",
+    },
+    code: 200,
+    status: "OK",
+	isError: false,
 };
 
+// 500
+res500 = {
+    response: null,
+    code: 500,
+    status: "Internal server error server error",
+	isError: true,
+};
+// 500
+res500_2 = {
+    response: {
+		message: {
+			error: "mongoerror: sadjkldjasklja"
+		}
+	},
+    code: 500,
+    status: "Internal server error server error",
+	isError: true,
+};
+
+
+
+// const {response, code, status} = dame.post("/algoCrm", {
+//     user: "asd",
+//     password: "asd",
+// }, {
+// 	config: "crm", // override config
+// });
+
+
+
+// const {response, code, status} = dame.post("https://api.neuro.com/dameDatos", {
+//     user: "asd",
+//     password: "asd",
+// }, {
+// 	useToken: false,
+// 	ignoreBaseUrl: true,
+// 	force: {
+// 		token: "token_neuro",
+// 	}
+// });
 
 
 
