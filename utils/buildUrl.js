1ce80282879d55b62fe100c443629f35dbbd7e46
Icaruk <adri-rk@hotmail.com>
2021-06-26 19:29:09 +0200

    fix

--- utils/buildUrl.js
+++ utils/buildUrl.js
@@ -1,7 +1,7 @@
 
 module.exports = function buildUrl(base, path) {
     
-	if (path.startsWith("http")) return path;
+	if (path.startsWith("http://") || path.startsWith("https://")) return path;
 	if (!base) return path;
 	
     return base + path;
