4e0c8db66beffc58cb960b0d47aa2f0079a57f70
Icaruk <adri-rk@hotmail.com>
2021-07-05 23:20:39 +0200

    buen build

--- build.js
+++ build.js
@@ -1,7 +1,10 @@
 require('esbuild').build({
 	entryPoints: ["lib/dame.js"],
 	bundle: true,
-	minify: true,
+	minifyWhitespace: true,
+	minifySyntax: true,
+	minifySyntax: true,
+	minifyIdentifiers: false,
 	format: "esm",
 	external: ["http", "https", "dns"],
 	outfile: 'index.js',
