45b7158065a71c3efcf3b26b114d4ed0f6454d86
Icaruk <adri-rk@hotmail.com>
2021-07-19 21:57:10 +0200

    build

--- build.js
+++ build.js
@@ -1,11 +1,12 @@
 require('esbuild').build({
-	entryPoints: ["lib/dame.js"],
+	entryPoints: [
+		"lib/dame.js",
+	],
 	bundle: true,
 	minifyWhitespace: true,
-	minifySyntax: true,
 	minifySyntax: true,
 	minifyIdentifiers: false,
 	format: "esm",
 	external: ["http", "https", "dns"],
-	outfile: 'index.js',
+	outdir: 'out',
 }).catch(() => process.exit(1))
