23fb56b5918c86144dc412271a43713d27802b6a
Icaruk <adri-rk@hotmail.com>
2021-08-02 10:41:09 +0200

    Fix tests

--- tests/fnc/get.js
+++ tests/fnc/get.js
@@ -6,12 +6,12 @@ exports.get_simple = async () => {
 	
 	test("get_simple", async () => {
 		
-		const dameRes = await dame.get("https://gorest.co.in/public/v1/users/2");
+		const dameRes = await dame.get("https://gorest.co.in/public/v1/users");
 		const {code, isError, response } = dameRes;
 		
 		expect(code).toBe(200);
 		expect(isError).toBe(false);
-		expect(response.data.email).toBe("patricia1234@gmail.com");
+		expect(response.meta.pagination.page).toBe(1);
 		
 	});
 	
@@ -26,12 +26,12 @@ exports.get_config_baseUrl = async () => {
 		dame.clearConfig("default");
 		dame.setConfig("default", "baseUrl", "https://gorest.co.in/public/v1");
 		
-		const dameRes = await dame.get("/users/2");
+		const dameRes = await dame.get("/users");
 		const {code, isError, response } = dameRes;
 		
 		expect(code).toBe(200);
 		expect(isError).toBe(false);
-		expect(response.data.email).toBe("patricia1234@gmail.com");
+		expect(response.meta.pagination.page).toBe(1);
 		
 	});
 	
@@ -43,14 +43,14 @@ exports.get_options_checkIsError = async () => {
 	
 	test("get_options_checkIsError", async () => {
 		
-		const dameRes = await dame.get("https://gorest.co.in/public/v1/users/2", {
+		const dameRes = await dame.get("https://gorest.co.in/public/v1/users", {
 			checkIsError: () => "abcd1234!",
 		});
 		const {code, isError, response } = dameRes;
 		
 		expect(code).toBe(200);
 		expect(isError).toBe("abcd1234!");
-		expect(response.data.email).toBe("patricia1234@gmail.com");
+		expect(response.meta.pagination.page).toBe(1);
 		
 	});
 	
@@ -65,12 +65,12 @@ exports.get_config_checkIsError = async () => {
 		dame.clearConfig("default");
 		dame.setConfig("default", "checkIsError", () => "abcd1234!");
 		
-		const dameRes = await dame.get("https://gorest.co.in/public/v1/users/2");
+		const dameRes = await dame.get("https://gorest.co.in/public/v1/users");
 		const {code, isError, response } = dameRes;
 		
 		expect(code).toBe(200);
 		expect(isError).toBe("abcd1234!");
-		expect(response.data.email).toBe("patricia1234@gmail.com");
+		expect(response.meta.pagination.page).toBe(1);
 		
 	});
 	
